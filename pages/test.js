773a16b324237f88db757a966c6f4c70365fee7d
gcgbarbosa <gcgbarbosa@gmail.com>
2020-09-16 10:03:49 -0700

    Add a simple interface to query the API

--- pages/test.js
+++ pages/test.js
@@ -1,204 +1,67 @@
-import Head from 'next/head'
-import axios from 'axios'
-
+import { useState } from "react";
+import Head from "next/head";
+import axios from "axios";
 
 const test = {
-   "opts":{
-      "sources":"tropicos,tpl,usda",
-      "class":"tropicos",
-      "mode":"resolve",
-      "matches":"best"
-   },
-   "data":[
-      [
-         1,
-         "Arecaceae Mauritia"
-      ]
-   ]
-}
-
-axios.post(
-  'http://vegbiendev.nceas.ucsb.edu:8975/tnrs_api.php', 
-  test, 
-  {headers:{'Content-Type':'application/json'}}
-).then((res) => {
-  console.log(res)
-}, (error) => {
-  console.log(error)  
-})
+  opts: {
+    sources: "tropicos,tpl,usda",
+    class: "tropicos",
+    mode: "resolve",
+    matches: "best",
+  },
+  // TODO: add more names
+  data: [],
+};
 
 export default function Test() {
+  const [result, setResult] = useState("");
+  const [jsonInput, setJsonInput] = useState("");
+  const [input, setInput] = useState("");
 
-
+  const queryNames = (names) => {
+    const query = names.split("\n").map((v, i) => [v, i + 1]);
+    test.data = query;
+    //test.data.push(query);
+    setJsonInput(JSON.stringify(test));
+    setResult("loading");
+    axios
+      .post("http://vegbiendev.nceas.ucsb.edu:8975/tnrs_api.php", test, {
+        headers: { "Content-Type": "application/json" },
+      })
+      .then(
+        (res) => {
+          console.log(res);
+          setResult(JSON.stringify(res));
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>TNRS Test</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
-        <h1 className="title">
-          test
-        </h1>
+        <h1 className="title">Test Request</h1>
+        <textarea
+          rows={10}
+          cols={50}
+          onChange={(e) => setInput(e.target.value)}
+        ></textarea>
+        <button type="button" onClick={() => queryNames(input)}>
+          Click Me!
+        </button>
+        <div>JsonInput: {jsonInput}</div>
+        <div>Result: {result}</div>
       </main>
-
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
+        __________
+        <br />
+        TNRS 2020
       </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
-  )
+  );
 }
