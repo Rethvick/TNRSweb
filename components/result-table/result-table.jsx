0349cf204d61e5790f5c3453bc14f3a6152ee9fe
Rohith Kumar Sajja <rohith.sajja@gmail.com>
2020-10-09 17:02:45 -0700

    Show dialog

--- components/result-table/result-table.jsx
+++ components/result-table/result-table.jsx
@@ -1,4 +1,4 @@
-//import { useState } from "react";
+import { useState } from "react";
 import { useStyles } from "./result-table.style";
 import {
   Paper,
@@ -8,14 +8,16 @@ import { Table } from "react-bootstrap";
 import BootstrapTable from "react-bootstrap-table-next";
 import paginationFactory from "react-bootstrap-table2-paginator";
 import * as ReactBootStrap from "react-bootstrap";
+import Popup from "../Popup/Popup.jsx";
 
 export function ResultTable({ tableData }) {
   const classes = useStyles();
+  const [openPopup, setOpenPopup] = useState(false);
   const renderTNRS = (name, index) => {
     return(
       <tr key={index}>
         <td>{name.ID}</td>
-        <td>{name.Name_submitted}</td> 
+        <td onClick={() => { setOpenPopup(true); }}>{name.Name_submitted}</td> 
         <td>{name.Name_matched}</td>
         <td><a href={name.Accepted_name_url}>{name.Source.toUpperCase()}</a></td>
         <td>{name.Overall_score}</td>
@@ -63,6 +65,14 @@ export function ResultTable({ tableData }) {
         columns={columns}
         pagination={paginationFactory()}
       />
+
+      <Popup
+        title="Employee Form"
+        openPopup={openPopup}
+        setOpenPopup={setOpenPopup}
+        children={tableData.map(renderTNRS)}
+      >
+      </Popup>
 
     </Paper>
     
