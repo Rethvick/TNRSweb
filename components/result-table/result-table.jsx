bbd2c3704ef000ba8e6eddadddabdaee4170ecc1
gcgbarbosa <gcgbarbosa@gmail.com>
2020-10-28 15:35:41 -0700

    Improve results table
    - Use loadash to generate links
    - Add missing columns for dialog box
    - Adjust size of dialog
    - Move toggler to select row to the left
    - Add author name to result table
    - Change table to material UI

--- components/result-table/result-table.jsx
+++ components/result-table/result-table.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
+import _ from "lodash";
 // import { useStyles } from "./result-table.style";
+//
+//
 import {
   Paper,
   Box,
@@ -18,43 +21,63 @@ import { Table } from "react-bootstrap";
 
 // import Popup from "../Popup/Popup.jsx";
 
+const mkLinks = (row) => {
+  // Name_matched_url
+  // Source
+  let sources = row.Source.split(",");
+  let links = row.Name_matched_url.split(";");
+  //
+  return _.zip(sources, links).map((pair) => (
+    <Link href="#" onClick={() => window.open(pair[1], "_blank")}>
+      {" "}
+      {pair[0].toUpperCase()}
+    </Link>
+  ));
+};
+
 function SelectRowDialog(props) {
   //
   const { onClose, open, rows, handleChangeSelectedRow } = props;
   //
   //
   return (
-    <Dialog aria-labelledby="dtitle" open={open}>
+    <Dialog aria-labelledby="dtitle" open={open} maxWidth="lg">
       <DialogTitle id="dtitle">Change selected name</DialogTitle>
       <Box m={4} mt={0}>
         <TableContainer>
           <Table aria-label="change selection table">
             <TableHead>
               <TableRow>
-                <TableCell>Name Submitted</TableCell>
+                <TableCell>Select</TableCell>
                 <TableCell>Name Matched</TableCell>
                 <TableCell>Source</TableCell>
                 <TableCell>Overall Score</TableCell>
-                <TableCell>Select</TableCell>
+                <TableCell>Author Matched</TableCell>
+                <TableCell>Author Score</TableCell>
+                <TableCell>Accepted Species</TableCell>
+                <TableCell>Unmatched Terms</TableCell>
+                <TableCell>Accepted Species</TableCell>
+                <TableCell>Taxonomic Status</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {rows.map((row) => (
                 <TableRow key={row.unique_id}>
-                  <TableCell>{row.Name_submitted}</TableCell>
-                  <TableCell>{row.Name_matched}</TableCell>
-                  <TableCell>
-                    <Link href={name.Accepted_name_url}>
-                      {row.Source.toUpperCase()}
-                    </Link>
-                  </TableCell>
-                  <TableCell>{row.Overall_score}</TableCell>
                   <TableCell>
                     <Switch
                       checked={row.selected}
                       onChange={() => handleChangeSelectedRow(row)}
                     />
                   </TableCell>
+                  <TableCell>{row.Name_matched}</TableCell>
+                  <TableCell>{mkLinks(row)}</TableCell>
+                  <TableCell>{row.Overall_score}</TableCell>
+                  <TableCell>{row.Author_matched}</TableCell>
+                  <TableCell>{row.Author_score}</TableCell>
+                  <TableCell>{row.Accepted_species}</TableCell>
+                  <TableCell>{row.Unmatched_terms}</TableCell>
+                  <TableCell>{row.Accepted_species}</TableCell>
+                  <TableCell>{row.Taxonomic_status}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
@@ -70,62 +93,67 @@ function SelectRowDialog(props) {
 
 // TODO: receive a call back function to set the id
 export function ResultTable({ tableData, onChangeSelectedRow }) {
-  // filter table data where selected == true
-  //
-  let tableDataSelected = tableData.filter((row) => row.selected == true);
+  //const classes = useStyles();
 
+  // filter table data where selected == true
+  let tableDataSelected = tableData.filter((row) => row.selected == true);
+  // filter rows by ID
   const getRows = (id) => {
     return tableData.filter((row) => row.ID == id);
   };
-
-  //const classes = useStyles();
+  // state
   const [popUpOpen, setPopUpOpen] = useState(false);
   const [popUpRows, setPopUpRows] = useState([]);
 
   const handleClickClose = () => {
     setPopUpOpen(false);
   };
-
-  const renderTNRS = (name, index) => {
+  const renderRow = (row, index) => {
     return (
-      <tr
-        key={index}
-        onClick={() => {
-          setPopUpRows(getRows(name.ID));
-          setPopUpOpen(true);
-        }}
-      >
-        <td>{name.ID}</td>
-        <td>{name.Name_submitted}</td>
-        <td>{name.Name_matched}</td>
-        <td>
-          <a href={name.Accepted_name_url}>{name.Source.toUpperCase()}</a>
-        </td>
-        <td>{name.Overall_score}</td>
-        <td>{name.Taxonomic_status}</td>
-        <td>{name.Accepted_name}</td>
-      </tr>
+      <TableRow key={index}>
+        <TableCell>{row.ID}</TableCell>
+        <TableCell>{row.Name_submitted} </TableCell>
+        <TableCell>
+          {row.Name_matched + " " + row.Accepted_name_author}{" "}
+          <Link
+            href="#"
+            onClick={() => {
+              setPopUpRows(getRows(row.ID));
+              setPopUpOpen(true);
+            }}
+          >
+            {"(+" + getRows(row.ID).length + " more)"}
+          </Link>
+        </TableCell>
+        <TableCell>{mkLinks(row)}</TableCell>
+        <TableCell>{row.Overall_score}</TableCell>
+        <TableCell>{row.Taxonomic_status}</TableCell>
+        <TableCell>{row.Accepted_name + " " + row.Accepted_name_author}</TableCell>
+      </TableRow>
     );
   };
 
   return (
     <>
-      <Paper>
-        <Table striped bordered hover responsive>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name Submitted</th>
-              <th>Name Matched</th>
-              <th>Source</th>
-              <th>Overall Score</th>
-              <th>Taxonomic Status</th>
-              <th>Accepted Name</th>
-            </tr>
-          </thead>
-          <tbody>{tableDataSelected.map(renderTNRS)}</tbody>
-        </Table>
-      </Paper>
+        <Box m={2} mt={0}>
+          <TableContainer>
+            <Table aria-label="change selection table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>ID</TableCell>
+                  <TableCell>Name Submitted</TableCell>
+                  <TableCell>Name Matched</TableCell>
+                  <TableCell>Source</TableCell>
+                  <TableCell>Overall Score</TableCell>
+                  <TableCell>Taxonomic Status</TableCell>
+                  <TableCell>Accepted Name</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>{tableDataSelected.map(renderRow)}</TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+
       <SelectRowDialog
         open={popUpOpen}
         onClose={handleClickClose}
