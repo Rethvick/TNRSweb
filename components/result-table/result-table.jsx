7ab51b7547b6643633ea8e53c0e89946ecddea36
gcgbarbosa <gcgbarbosa@gmail.com>
2020-11-12 18:59:36 -0700

    Add the other half of paginaation

--- components/result-table/result-table.jsx
+++ components/result-table/result-table.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 import _ from "lodash";
 
-import { WarningTwoTone as WarningTwoToneIcon } from "@material-ui/icons";
+import {
+  WarningTwoTone as WarningTwoToneIcon,
+  FirstPage as FirstPageIcon,
+  LastPage as LastPageIcon,
+  KeyboardArrowLeft,
+  KeyboardArrowRight,
+} from "@material-ui/icons";
 //
 import {
   Box,
@@ -13,6 +19,8 @@ import {
   Table,
   TablePagination,
   Link,
+  IconButton,
+  TextField,
 } from "@material-ui/core";
 
 import { WarningsPopover } from "./warnings";
@@ -22,6 +30,91 @@ import { mkSourceLinks, mkAcceptedNameLinks } from "./links";
 import { roundScore } from "../../src/actions";
 
 // import Popup from "../Popup/Popup.jsx";
+
+function TablePaginationActions(props) {
+  const { count, page, rowsPerPage, onChangePage } = props;
+  const [inputPage, setInputPage] = useState(page);
+
+  const handleFirstPageButtonClick = (event) => {
+    onChangePage(event, 0);
+    setInputPage(1);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onChangePage(event, page - 1);
+    setInputPage(page);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onChangePage(event, page + 1);
+    setInputPage(page + 2);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    setInputPage(Math.max(0, Math.ceil(count / rowsPerPage)));
+  };
+
+  return (
+    <>
+      <Box display="flex">
+        <IconButton
+          onClick={handleFirstPageButtonClick}
+          disabled={page === 0}
+          aria-label="first page"
+        >
+          <FirstPageIcon />
+        </IconButton>
+        <IconButton
+          onClick={handleBackButtonClick}
+          disabled={page === 0}
+          aria-label="previous page"
+        >
+          <KeyboardArrowLeft />
+        </IconButton>
+      </Box>
+      <Box width={150}>
+        <TextField
+          value={inputPage}
+          onKeyDown={(e) => {
+            // when the user presses enter key=13
+            if (e.keyCode === 13) {
+              let maxPage = Math.max(0, Math.ceil(count / rowsPerPage) - 1);
+              if (inputPage < 1) {
+                onChangePage(e, 0);
+                setInputPage(1);
+              } else if (inputPage > maxPage) {
+                onChangePage(e, maxPage);
+                setInputPage(maxPage+1);
+              } else {
+                onChangePage(e, inputPage-1);
+              }
+            }
+          }}
+          onChange={(e) => setInputPage(e.target.value)}
+          variant="outlined"
+          size="small"
+        />
+      </Box>
+      <Box display="flex">
+        <IconButton
+          onClick={handleNextButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="next page"
+        >
+          <KeyboardArrowRight />
+        </IconButton>
+        <IconButton
+          onClick={handleLastPageButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="last page"
+        >
+          <LastPageIcon />
+        </IconButton>
+      </Box>
+    </>
+  );
+}
 
 export function ResultTable({ tableData, onChangeSelectedRow }) {
   // filter table data where selected == true
@@ -137,6 +230,7 @@ export function ResultTable({ tableData, onChangeSelectedRow }) {
             page={page}
             onChangePage={handleChangePage}
             onChangeRowsPerPage={handleChangeRowsPerPage}
+            ActionsComponent={TablePaginationActions}
           />
         </TableContainer>
       </Box>
