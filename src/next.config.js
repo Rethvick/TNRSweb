a347fdc7392ea404828feb81184c880ae2bced71
Brad Boyle <bboyle@email.arizona.edu>
2022-01-28 15:22:03 -0700

    Updates to support TNRS 5.0 and TNRS DB 4.2

---
+++ src/next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  trailingSlash: true,
+  env: {
+    // development
+   // apiServer: "http://vegbiendev.nceas.ucsb.edu:8975/",
+    // production
+    apiServer: "https://tnrsapi.xyz/",
+    // development
+    //apiEndPoint: "http://vegbiendev.nceas.ucsb.edu:8975/tnrs_api.php",
+    // production
+    apiEndPoint: "https://tnrsapi.xyz/tnrs_api.php",
+    defaultMatchingThreshold: .53
+  },
+};
