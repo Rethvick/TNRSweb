8bc9c1a41cab8526f4260f833573b0980275947d
Brad Boyle <bboyle@email.arizona.edu>
2022-01-28 16:25:53 -0700

    Test switch to dev api

--- src/next.config.js
+++ src/next.config.js
@@ -2,13 +2,13 @@ module.exports = {
   trailingSlash: true,
   env: {
     // development
-   // apiServer: "http://vegbiendev.nceas.ucsb.edu:8975/",
+   apiServer: "http://vegbiendev.nceas.ucsb.edu:8975/",
     // production
-    apiServer: "https://tnrsapi.xyz/",
+    //apiServer: "https://tnrsapi.xyz/",
     // development
-    //apiEndPoint: "http://vegbiendev.nceas.ucsb.edu:8975/tnrs_api.php",
+	apiEndPoint: "http://vegbiendev.nceas.ucsb.edu:8975/tnrs_api.php",
     // production
-    apiEndPoint: "https://tnrsapi.xyz/tnrs_api.php",
+    //apiEndPoint: "https://tnrsapi.xyz/tnrs_api.php",
     defaultMatchingThreshold: .53
   },
 };
