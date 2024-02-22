7879952f5793a58c5e3f26c8967116eaf2d0f7cb
rethvick <furyassassinofficial@gmail.com>
2024-02-22 09:42:21 -0800

    Redirecting to Sources page and Enhancement update

--- components/options-box/options-box.jsx
+++ components/options-box/options-box.jsx
@@ -17,11 +17,11 @@ import {
 import { requestSources, requestFamilyClassifications } from "../../actions";
 
 export function OptionsBox({
-  queryType,
-  onChangeQueryType,
-  onChangeSources,
-  onChangeFamily,
-}) {
+                             queryType,
+                             onChangeQueryType,
+                             onChangeSources,
+                             onChangeFamily,
+                           }) {
   const classes = useStyles();
 
   // sources and familiesAvailable
@@ -31,24 +31,30 @@ export function OptionsBox({
   // populate souce state with sources available
   let [sourcesState, setSourcesState] = useState([]);
 
+  const [selectedSource, setSelectedSource] = useState("");
+
   useEffect(() => {
     async function fetchData() {
       let sources = await requestSources();
       // get only the names
-      let sourceNames = sources.map((s) => s.sourceName);
       let families = await requestFamilyClassifications();
 
       setSourcesState(
-        sourceNames.map((name) => {
-          return { name: name, enabled: true };
-        })
+          sources.map((source) => ({
+            name: source.sourceName,
+            enabled: source.isDefault === "1"
+          }))
       );
 
       setFamiliesAvailable(families);
       setFamilyQuery(families[0].sourceName);
 
       // push names up to index
-      onChangeSources(sourceNames.join(","));
+      let enabledSources = sources
+          .filter((s) => s.isDefault === "1")
+          .map((s) => s.sourceName)
+          .join(",");
+      onChangeSources(enabledSources);
       onChangeFamily(families[0].sourceName);
     }
 
@@ -58,84 +64,102 @@ export function OptionsBox({
   // controls the behavior of the user when he clicks the switch
   const handleChangeSources = (name) => {
     let tmpSourcesState = sourcesState.map((source) => {
-      if (source.name == name) {
-        // flip switch
+      if (source.name === name) {
         source.enabled = !source.enabled;
+        setSelectedSource(source.enabled ? name : "");
+        if (source.enabled) {
+          const element = document.getElementById(`source-${name.toLowerCase()}`);
+          if (element) {
+            element.scrollIntoView({ behavior: "smooth" });
+          }
+        }
       }
       return source;
     });
 
     setSourcesState(tmpSourcesState);
 
-    // send result to the index page
     let sourceNames = tmpSourcesState
-      .filter((s) => s.enabled)
-      .map((s) => s.name)
-      .join(",");
+        .filter((s) => s.enabled)
+        .map((s) => s.name)
+        .join(",");
     onChangeSources(sourceNames);
   };
 
   const handleSelectFamily = (name) => {
     onChangeFamily(name);
     setFamilyQuery(name);
+    setSelectedSource("");
   };
 
+
   return (
-    <Paper className={classes.paper}>
-      <Box p={2}>
-        <Box>
-          <InputLabel>Processing Mode</InputLabel>
-          <FormControl variant="outlined" fullWidth>
-            <Select
-              value={queryType}
-              onChange={(e) => onChangeQueryType(e.target.value)}
-            >
-              <MenuItem value={"resolve"}>Perform name resolution</MenuItem>
-              <MenuItem value={"parse"}>Parse names only</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-        {queryType === "resolve" && (
-          <>
-            <Box pt={2}>
-              <InputLabel>Family Classification</InputLabel>
-              <FormControl variant="outlined" fullWidth>
-                <Select
-                  value={familyQuery}
-                  onChange={(e) => handleSelectFamily(e.target.value)}
-                >
-                  {familiesAvailable.map((f) => (
-                    <MenuItem key={f.sourceName} value={f.sourceName}>
-                      {f.sourceName.toUpperCase()}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Box>
-            <Box pt={2}>
-              <FormLabel component="legend">
-                <Link href="/sources">Sources</Link>
-              </FormLabel>
-              <FormGroup row>
-                {sourcesState?.map((s) => {
-                  return (
-                    <FormControlLabel
-                      key={s.name}
-                      control={
-                        <Switch
-                          onClick={() => handleChangeSources(s.name)}
-                          checked={s.enabled}
-                        />
+      <Paper className={classes.paper}>
+        <Box p={2}>
+          <Box>
+            <InputLabel>Processing Mode</InputLabel>
+            <FormControl variant="outlined" fullWidth>
+              <Select
+                  value={queryType}
+                  onChange={(e) => onChangeQueryType(e.target.value)}
+              >
+                <MenuItem value={"resolve"}>Perform name resolution</MenuItem>
+                <MenuItem value={"parse"}>Parse names only</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+          {queryType === "resolve" && (
+              <>
+                <Box pt={2}>
+                  <InputLabel>Family Classification</InputLabel>
+                  <FormControl variant="outlined" fullWidth>
+                    <Select
+                        value={familyQuery}
+                        onChange={(e) => handleSelectFamily(e.target.value)}
+                    >
+                      {familiesAvailable.map((f) => (
+                          <MenuItem key={f.sourceName} value={f.sourceName}>
+                            {f.sourceName.toUpperCase()}
+                          </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </Box>
+                <Box pt={2}>
+                  <FormLabel component="legend">
+                    <Link href="/sources">Taxonomic Sources</Link>
+                  </FormLabel>
+                  <FormGroup row>
+                    {sourcesState?.map((s) => {
+                      if (s.name){
+                        return (
+                            <FormControlLabel
+                                key={s.name}
+                                control={
+                                  <Switch
+                                      onClick={() => handleChangeSources(s.name)}
+                                      checked={s.enabled}
+                                  />
+                                }
+                                label={
+                                  <Link
+                                      href={`/sources#source-${s.name.toLowerCase()}`}
+                                      onClick={() => setSelectedSource(s.name)}
+                                      underline={selectedSource === s.name ? "always" : "hover"}
+                                  >
+                                    {s.name.toUpperCase()}
+                                  </Link>
+                                }
+                            />
+                        );
                       }
-                      label={s.name.toUpperCase()}
-                    />
-                  );
-                })}
-              </FormGroup>
-            </Box>
-          </>
-        )}
-      </Box>
-    </Paper>
+                      return null;
+                    })}
+                  </FormGroup>
+                </Box>
+              </>
+          )}
+        </Box>
+      </Paper>
   );
 }
